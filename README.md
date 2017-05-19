48bf64896be38b892538c9314428efaf8e8d69c5
emrep <piskin.emre@gmail.com>
2017-05-19 23:20:21 +0300

    Adding project files

--- README.md
+++ README.md
@@ -1,19 +1,26 @@
 # Eksi Sade addon for Firefox
 
-[![Mozilla Add-on](https://img.shields.io/amo/users/eksi-sade.svg?style=flat-square)](https://addons.mozilla.org/en-US/firefox/addon/eksi-sade) [![Mozilla Add-on](https://img.shields.io/amo/rating/eksi-sade.svg?style=flat-square)](https://addons.mozilla.org/en-US/firefox/addon/eksi-sade) [![Mozilla Add-on](https://img.shields.io/amo/d/eksi-sade.svg?style=flat-square)](https://addons.mozilla.org/en-US/firefox/addon/eksi-sade) [![Mozilla Add-on](https://img.shields.io/amo/v/eksi-sade.svg?style=flat-square)](https://addons.mozilla.org/en-US/firefox/addon/eksi-sade)
+[![Mozilla Add-on](https://img.shields.io/amo/users/eksi-sade.svg?style=flat-square)](https://addons.mozilla.org/addon/eksi-sade) [![Mozilla Add-on](https://img.shields.io/amo/rating/eksi-sade.svg?style=flat-square)](https://addons.mozilla.org/addon/eksi-sade) [![Mozilla Add-on](https://img.shields.io/amo/d/eksi-sade.svg?style=flat-square)](https://addons.mozilla.org/addon/eksi-sade) [![Mozilla Add-on](https://img.shields.io/amo/v/eksi-sade.svg?style=flat-square)](https://addons.mozilla.org/addon/eksi-sade)
 
-Ekşi Sözlük simplified. Removes many parts, colourises topic links, brings a dark theme for the famous Turkish social network.
+Ekşi Sözlük simplified. A Firefox addon for the famous Turkish social network.
+
+* `Simple`: Removes many parts, scripts, including ads
+* `Troll detection`: Marks troll comments with a red background 
+  * The troll list is mainly based on this [entry]
+* `Colored topics`: Colourises topic links and sorts them descending order
+* `Bitter`: Brings a dark theme
 
 An ad blocker addon like uBlock Origin is also recommended for faster surfing.
+
+[entry]: https://eksisozluk.com/basliklari-engellenecek-buyuk-aktroll-listesi--5229083
 
 ## Screenshot
 ![Screenshot](screenshot.png?raw=true "Screenshot")
 
 ## Usage
-* Install the latest release from [https://addons.mozilla.org/en-US/firefox/addon/eksi-sade](https://addons.mozilla.org/en-US/firefox/addon/eksi-sade)
-* Activate the addon here: [about:addons](about:addons)
-* Go to Ekşi Sözlük website: [https://eksisozluk.com](https://eksisozluk.com)
-* The addon will be activated after page load
+* Install the latest release from [https://addons.mozilla.org/addon/eksi-sade](https://addons.mozilla.org/addon/eksi-sade)
+* Activate the addon from `Tools > Addons` menu
+* Now you can visit the new Ekşi Sözlük: [https://eksisozluk.com](https://eksisozluk.com)
 
 ## Reporting Bugs
 For bug reports, questions, feature requests, or other suggestions the best way to contact me is to [create an issue][newissue] on GitHub.
@@ -25,3 +32,14 @@ Make the plugin better! Join the [contributors] today by submitting a patch! The
 
 [contributors]: https://github.com/pemre/eksi-sade/graphs/contributors
 [fork]: https://github.com/pemre/eksi-sade/fork
+
+## Changelog
+* v1.1.0
+  * Troll detection feature added
+  * Performance improved using CSS file instead of jQuery
+  * Top menu restored
+  * Left menu fixed for Android
+  * Added custom logo
+  * Link of Ekşi Şeyler removed
+  * The more link at the bottom for Gündem restored
+* v1.0.0 Initial release
--- eksi-sade.js
+++ eksi-sade.js
@@ -1,95 +1,263 @@
-/**
- * Removes unnecessary parts
- *
- * @param list
- */
-function removeParts() {
-    // List of selectors to remove
-    var removeList = [
-        'header',
-        'aside',
-        '#site-footer',
-        '.ads',
-        'iframe',
-        'script',
-        'noscript',
-        '.quick-index-continue-link-container'
-    ];
-    // Remove them
-    $(removeList.join(",")).remove();
-    // Remove next siblings of the container
-    $('#container').nextAll().remove();
-    //removeSelectorSiblings("#container");
-}
-/**
- * Applies a dark theme
- */
-function applyTheme() {
-    $('body')
-        .css('padding', '0')                            // Pull the rest of the site to top
-        .css('background-color', '#333')                // Dark background
-        .css('color', '#ddd');                          // Light text
-    $('#index-section').css('top', '0');                // Pull the rest of the site to top
-    $('a').css('color', '#53a245');                     // Green links
-    $('#main').css('margin-left', '370px');             // Wider side menu
-    $('#index-section').css('width', '370px');
-    $('.topic-list > li > a').css('padding', '3px 0');  // Compact side menu links
-}
+'use strict';
+
 /**
  * Sorts and colourises side menu links
  */
 function improveLinks() {
-// Remove sponsored links
+    // Remove sponsored links
     $('.topic-list li[id*=sponsored]').remove();
 
     $('.topic-list a').each(function() {
-        //$(this).attr('href', 'https://eksisozluk.com' + $(this).attr('href'));
-        //$(this).attr('title', $(this).text());
-        // Crop long texts
-        //textObj = $(this).contents().first()[0];
-        //if (textObj.textContent.length > 46)
-        //    textObj.textContent = textObj.textContent.slice(0, -4) + '.. ';
+        var t = $(this);
+
+        /*// Relative url fix
+        t.attr('href', 'https://eksisozluk.com' + t.attr('href'));
+        t.attr('title', t.text());*/
+
+        /*// Crop long texts
+        var textObj = t.contents().first()[0];
+        if (textObj.textContent.length > 46)
+            textObj.textContent = textObj.textContent.slice(0, -4) + '.. ';*/
 
         // Rank can be like "2,2b", convert it into numbers
-        rankText = $(this).contents().last()[0].textContent;
-        if (rankText.substr(-1) == 'b') {
+        var rankText = t.contents().last()[0].textContent;
+        if (rankText.substr(-1) === 'b') {
             rankText = rankText.substring(0, rankText.length - 1);
             if (rankText.indexOf(',') >= 0)
                 rankText = rankText.replace(',', '') + "00";
             else
                 rankText += "000";
-            $(this).contents().last()[0].textContent = rankText;
+            t.contents().last()[0].textContent = rankText;
         }
 
         // Give colors
-        rank = Number(rankText);
-        if (rank > 1000)     $(this).css('color', '#ff0000');
-        else if (rank > 500) $(this).css('color', '#ff8000');
-        else if (rank > 300) $(this).css('color', '#ffbf00');
-        else if (rank > 200) $(this).css('color', '#ffff00');
-        else if (rank > 100) $(this).css('color', '#bfff00');
-        else if (rank > 80)  $(this).css('color', '#55ff00');
-        else if (rank > 60)  $(this).css('color', '#00ff55');
-        else if (rank > 40)  $(this).css('color', '#00ffbf');
-        else if (rank > 20)  $(this).css('color', '#00bfff');
-        else if (rank > 10)  $(this).css('color', '#0088bb');
-        else if (rank > 0)   $(this).css('color', '#5599aa');
+        var rank = Number(rankText);
+        if (rank > 1000)     t.css('color', '#ff0000');
+        else if (rank > 500) t.css('color', '#ff8000');
+        else if (rank > 300) t.css('color', '#ffbf00');
+        else if (rank > 200) t.css('color', '#ffff00');
+        else if (rank > 100) t.css('color', '#bfff00');
+        else if (rank > 80)  t.css('color', '#55ff00');
+        else if (rank > 60)  t.css('color', '#00ff55');
+        else if (rank > 40)  t.css('color', '#00ffbf');
+        else if (rank > 20)  t.css('color', '#00bfff');
+        else if (rank > 10)  t.css('color', '#0088bb');
+        else if (rank > 0)   t.css('color', '#5599aa');
     });
 
     // Sort function callback
     var sortLi = function (a, b) {
-        na = Number($(a).children().first().children().first().html());
-        nb = Number($(b).children().first().children().first().html());
+        var na = Number($(a).children().first().children().first().html());
+        var nb = Number($(b).children().first().children().first().html());
         return nb > na ? 1 : -1;
     };
 
     $('.topic-list li').sort(sortLi)    // sort elements
         .appendTo('.topic-list');       // append again to the list
 }
+/**
+ * Warns against troll accounts by giving a red background to their comments
+ */
+function trollWarning() {
+    var trollList = [
+        // 16 kasım 2016 eklenenler
+        "lord eddard stark",
+        'dudayeva',
+        'dinleyin ulan develer',
+        'sick city',
+        'rich peach',
+        'the way we were',
+        'hababam vokal grubu',
+        'aab',
+        'c2h5oh',
+        'ibrahim tasteless',
+        'popilikadam',
+        'vanhohenheim',
+        'balbarss',
+        'comenger',
+        'anti duhring',
+        'ali bey amca',
+        'kendini arayan adamin kendi',
+        'yurtsevenfive',
+        'proust',
+        'hukuk ordinaryusu',
+        'bill gates benim babam tamam mi',
+        'said kotan',
+        'this one',
+        'gauldoth',
+        'rampris2',
+        'ivan drago',
+        // 17 kasım 2016
+        'writer on the water',
+        'yusufnalkesen',
+        'iqm',
+        'soz buyugun',
+        'simurg anka',
+        'ormanci',
+        'yazar olmam engellenemez',
+        'ne nicki be',
+        'samatya',
+        'bu saatte',
+        '20 13',
+        'pagan papagan',
+        'mr and mrs brown',
+        'kasimi severiz biz o baska bir guzeldir',
+        'parisa',
+        'tekid',
+        // 15 mart 2017
+        'optumsaygoodbye',
+        'sakalli vasfi',
+        'han hazretleri',
+        'medellin',
+        'liberteryen',
+        // from other users
+        'yapcak-bisi-yok',
+        'fikirmuhendisifikri',
+        // https://eksisozluk.com/entry/64141929
+        'boldpilot',
+        'burun mu o',
+        'bymuss',
+        'ehriman',
+        'feel 1337',
+        'galahad',
+        'hayk0cepkin',
+        'irfangerekli',
+        'ponya',
+        'presenceofthedarklord',
+        'slv ard fc mrn die atl flc dgz pra m ass',
+        'taksici milletin efendisidir',
+        'these are the days of our lives',
+        // trollspray
+        'yenilikci dervis',
+        'hepiniz oleceksiniz',
+        'nerede o eski bayramlar',
+        'buraya kadar gelmek',
+        'mest ustune mesh',
+        'salih bulut',
+        'pagan papagan',
+        'bynewyorker',
+        'sebasteia sebasteia',
+        'gok yeleli bozkurt',
+        'afrocubanbebop',
+        'iyi biri',
+        'jasis',
+        'hasbelicab',
+        'iett 99 806',
+        'onthebass',
+        'paegan mushroom',
+        'detroitli kizil',
+        'bdeveci',
+        'peder hose',
+        'noryth aquanum',
+        'don kulot',
+        'kardanadam7853',
+        'auer',
+        'aziz vefa',
+        'somek kafali',
+        'the irlandali',
+        'atmaa',
+        'redlack',
+        'idontgiveafuck',
+        'ic benim icin',
+        'cenikli',
+        'owencan',
+        'edwardscissorhand',
+        'musanelka',
+        'kimmugirl',
+        'dont let it get to you',
+        'emin',
+        'naftalinli don',
+        'vincent ugurlu',
+        'kizilay 404',
+        'evetisyan',
+        'multiple intelligentiis',
+        'noxan',
+        'sergey abdulkaramazov',
+        'men kucik chomichmen',
+        'speranza',
+        'ippolit kirillovic',
+        'and justice erol',
+        'beyaz adam',
+        'baso',
+        'ed statik',
+        'montmartre sakini',
+        'karisik meyve aromali gazoz',
+        'gettin crunk',
+        'kuzeysanrisigibidirgeceyibesefilanboler',
+        'kartoncu',
+        'evde black smoke besleyen adam',
+        'submarine solo',
+        'kaptan maydanoz',
+        'bir gurultu aninda',
+        'parabola',
+        'ozzy osporny',
+        'karabetgulludere',
+        'gael garcia bernalin ikizi',
+        'yvzerg',
+        'excaliboor',
+        'bir cevabim yok ama soru guzelmis',
+        'neseli ispanak',
+        'at yalani sikeyim inanani',
+        'iknowyouwantme',
+        'gogdabor',
+        'burdan post modern nicklere selam ederim',
+        'cornivolk',
+        'ledallamafrench',
+        'eksibok',
+        'srfydn',
+        'cok bilinmeyenli denklem',
+        // Other
+        'nick bulmam lazim',
+        'michael owen',
+        'rke',
+        'asilcanarkadasbenim',
+        'behlul olarak giris yapmak istiyorum',
+        'omeremre42'
+    ];
+
+    $('li[data-author]').each(function() {
+        var t = $(this);
+        if($.inArray(t.data('author'), trollList) !== -1) {
+            t.css('background-color', '#533')
+        }
+    });
+}
+/**
+ * Creates an event to improve links on every top menu click (Ajax call)
+ */
+function improveLinksAddEvent() {
+  $('#partial-index').on('DOMNodeInserted', function(e) {
+    // Check if the node contains the topic list
+    if (e.target.classList.value === "topic-list partial") {
+      improveLinks();
+    }
+  });
+}
+/**
+ * Removes unnecessary parts. CSS hiding is not enough! :)
+ */
+function removeParts() {
+  // List of selectors to remove
+  var removeList = [
+    'aside',
+    '#site-footer',
+    '.ads',
+    'iframe',
+    'script',
+    'noscript',
+    '.eksiseyler-logo'
+  ];
+  // Remove them
+  $(removeList.join(",")).remove();
+  // Remove next siblings of the container
+  $('#container').nextAll().remove();
+  //removeSelectorSiblings("#container");
+}
 
 /**
  * Start process
  */
-removeParts();
-applyTheme();
 improveLinks();
+improveLinksAddEvent();
+trollWarning();
+removeParts();
